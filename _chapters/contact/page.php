79993cbce4afd5ed689a01b3f3f57eecca421de9
megagustav <g.moorhouse@me.com>
2018-11-18 16:46:36 +0100

    Add files via upload

---
+++ _chapters/contact/page.php
@@ -0,0 +1,21 @@
+<div class="container-fluid chapter mx-0 mb-2 mb-md-3 pb-5 pt-5" id="<?php echo $chapter_list[$i][2]; ?>">
+
+<div class="row">
+  <div class="col-xl-8 col-lg-10 shadow-lg py-4 px-5 mb-5">
+    <div class="card noborder">
+      <div class="card-body pb-2">
+		<h2 class="card-title display-3 pt-2 pb-0 mb-0">i hope you enjoyed this.</h2>
+		<h2 class="card-text font-weight-light pb-3">let's work together</h2>
+
+        <a href="mailto:hello@me.com?subject=Portfolio%20for%20<?php echo $company; ?>" target="_blank" class="btn btn-primary mb-4">hello@me.com</a>
+
+        <a href="sms:0123456789" target="_blank" class="btn btn-primary mb-4">0123456789</a>
+
+        <a href="<?php echo $portfolio_dl; ?>" target="_blank" class="btn btn-primary mb-4"><i class="fas fa-arrow-alt-to-bottom"></i> Portfolio</a>
+
+        <a href="<?php echo $cv_dl; ?>" target="_blank" class="btn btn-primary mb-4"><i class="fas fa-arrow-alt-to-bottom"></i>  CV</a>
+      </div>
+    </div>
+  </div>
+
+</div>

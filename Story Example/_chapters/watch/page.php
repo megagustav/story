97bc1e1dffe430de1b510fb25001e615b6d7e12c
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-02 16:20:58 +0100

    reuploading

---
+++ Story Example/_chapters/watch/page.php
@@ -0,0 +1,91 @@
+<div class="chapter" id="<?php echo $chapter_list[$i][2]; ?>">
+	<div class="container-fluid mx-0 px-0 chapter-begin">
+		<div class="row no-gutters mb-5 headline">
+			<div class="col-12 px-3 px-md-5 mb-3 mb-md-5">
+				<h2 class="display-3 font-weight-bold">Watch by Slate</h2>
+			</div>
+			<div class="col-12 col-md-6 px-3 px-md-5 mb-5">
+				<p class="h4 text-muted font-weight-light">Studio Slate Werner</p>
+				<p class="h4 text-muted font-weight-light">San Francisco, CA</p>
+				<p class="h4 text-muted font-weight-light">Winter 2017</p>
+				<div class="mt-3 mt-md-5">
+					<a class="btn btn-primary mb-2" href="/_chapters/watch/Watch by Slate.wire" target="_blank" download="Mirror-Bar.wire">
+						<i class="fas fa-pencil-ruler" style="margin-right: 0.75rem;"></i>Model.wire</a><br>
+					<a class="btn btn-primary" href="/_chapters/watch/Watch by Slate.pdf" target="_blank" download="Mirror-Bar.pdf">
+						<i class="fas fa-file-pdf" style="margin-right: 1rem;"></i>Documentation.pdf</a>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 px-3 pl-md-0 pr-md-5 mb-5" >
+				<p class="h4 font-weight-light" style="line-height: 30px;">
+					The Watch can only be described by the graceful curves that blend
+					from glass to metal.<br><br>
+
+					This project is a technical modeling exercise that was preformed as
+					part of an application to the Apple CAD Sculpting Team. A physical Watch
+					was inspected closely, modeled in Alias, and rendered in Keyshot.<br><br>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mx-0 px-0 spread-gallery">
+		<div class="row no-gutters">
+			<div class="col-md-12 mb-3" style="padding-left: 0px; padding-right: 0px">
+				<img class="hero" src="/_chapters/watch/watch_00001.jpg" alt="" style="width: 100%;"/>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mx-0 px-0 spread-gallery">
+		<div class="row no-gutters">
+			<div class="col-md ">
+			    <div class="masonry gallery gap-wide">
+						<!-- first column -->
+						<img class="mb-3 gap-wide first-image" src="/_chapters/watch/watch_00004.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00003.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00016.png" alt=""/>
+			      <!-- second column -->
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00015.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00002.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00017.png" alt=""/>
+			    </div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mx-0 px-0 spread-gallery">
+		<div class="row no-gutters">
+			<div class="col-md-12 mb-3" style="padding-left: 0px; padding-right: 0px">
+				<img class="hero" src="/_chapters/watch/watch_00019.png" alt="" style="width: 100%;"/>
+			</div>
+		</div>
+		<div class="row no-gutters">
+			<div class="col-md-12 mb-3" style="padding-left: 0px; padding-right: 0px">
+				<img class="hero" src="/_chapters/watch/watch_00010.png" alt="" style="width: 100%;"/>
+			</div>
+		</div>
+		<div class="row no-gutters">
+			<div class="col-md-12 mb-3" style="padding-left: 0px; padding-right: 0px">
+				<img class="hero" src="/_chapters/watch/watch_00008.png" alt="" style="width: 100%;"/>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mx-0 px-0 spread-gallery">
+		<div class="row no-gutters">
+			<div class="col-md ">
+			    <div class="masonry gallery gap-wide">
+						<!-- first column -->
+						<img class="mb-3 gap-wide first-image" src="/_chapters/watch/watch_00011.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00010.png" alt=""/>
+			      <!-- second column -->
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00012.png" alt=""/>
+						<img class="mb-3 gap-wide" src="/_chapters/watch/watch_00013.png" alt=""/>
+			    </div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mx-0 px-0 chapter-end">
+		<div class="row no-gutters">
+			<div class="col-12 mt-3 mt-md-5 d-block" >
+				<p class="display-4 font-weight-light text-secondary text-center">—</p>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ Story Example/project-page/settings.php
@@ -0,0 +1,21 @@
+<?php
+// Settings for Story for
+$published     = "June 2019";
+$title         = "Watch by Slate";
+$description   = "Sculpting the Apple Watch";
+$title_left    = "Studio Slate Werner";
+$title_right   = "";
+$bottom_left   = "";
+$bottom_right  = "";
+
+// This image is refferenced in the meta tag
+$preview_image = "https://slate.werner.studio/_chapters/watch/watch_00010.png";
+
+// _chapters
+// FORMAT: "Chapter Title", "link to chapter", "URL friendly title"
+$chapter_list  = array(
+    array("Watch by Slate", "../_chapters/watch/page.php",  "watch"),
+
+);
+$chapter_count = count($chapter_list);
+?>

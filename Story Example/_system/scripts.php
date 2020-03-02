6aaac31af8e915aa85dfed55e5897d59f8f7d6b9
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-02 16:40:14 +0100

    update for portfolio2020

--- Story Example/_system/scripts.php
+++
@@ -1,4 +0,0 @@
-<!-- jQuery, Popper.js, Bootstrap -->
-<script src="../_system/js/jquery-3.2.1.slim.min.js"></script>
-<script src="../_system/js/popper.js"></script>
-<script src="../_system/js/bootstrap.min.js"></script>

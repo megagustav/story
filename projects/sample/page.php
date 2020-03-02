a6f91edef345e043c92632ec43ffa5a16321eb40
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-02 17:02:04 +0100

    cleaning up

---
+++ projects/sample/page.php
@@ -0,0 +1,60 @@
+<div class="chapter" id="<?php echo $chapter_list[$i][2]; ?>">
+
+	<!-- START INTRODUCTION -->
+
+	<div class="container-fluid mx-0 px-0 chapter-begin">
+		<div class="row no-gutters mb-5 headline">
+		
+			<div class="col-12 px-3 px-md-5 mb-3 mb-md-5">
+				<h2 class="display-3 font-weight-bold">World Saving Thing</h2>
+			</div>
+			
+			<div class="col-12 col-md-6 px-3 px-md-5 mb-5">
+				<p class="h4 text-muted font-weight-light">Design Company</p>
+				<p class="h4 text-muted font-weight-light">Berlin</p>
+				<div class="mt-3 mt-md-5">
+					<a class="btn btn-primary mr-3" href="#" target="_blank" download="Presentation.pdf"><i class="fas fa-file-pdf"></i> Presentation (5.5 MB)</a>
+				</div>
+			</div>
+			
+			<div class="col-12 col-md-6 px-3 pl-md-0 pr-md-5 mb-5" >
+				<p class="h4 font-weight-light">
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. <br><br>
+
+Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <br><br>
+
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				</p>
+			</div>
+			
+		</div>
+	</div>
+	
+	<!-- END INTRODUCTION -->
+	
+	<div class="container-fluid mx-0 px-0 spread-gallery">
+		<div class="row no-gutters">
+		
+			<div class="col-md-12 mb-3">
+				<img class="hero" src="../../projects/sample/placeholder.png" alt=""/>
+			</div>
+			
+			<div class="col-md-12">
+				<div class="masonry gallery gap-wide">
+					<!-- first column -->
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<!-- second column -->
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+					<img class="mb-3 gap-wide" src="../../projects/sample/placeholder.png" alt=""/>
+				</div>
+			</div>
+			
+		</div><!-- ROW GALLERY-->
+		
+	</div><!-- CONTAINER -->
+	
+</div>

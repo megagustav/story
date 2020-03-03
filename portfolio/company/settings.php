fc79e1ada1abc48a6c61bbd49167a999556c28fd
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-03 01:11:30 +0100

    made letter.txt and settings anon

--- portfolio/company/settings.php
+++ portfolio/company/settings.php
@@ -2,22 +2,20 @@
 // Settings for Story for
 $published     = "June 2019";
 $title         = "Gustav Moorhouse – Portfolio for Company";
-$description   = "Sculpting the Apple Watch";
-$title_left    = "back to moor.house";
+$description   = "";
+$title_left    = "back to website";
 $title_right   = "Portfolio for Company";
 $bottom_left   = "Gustav Moorhouse";
 $bottom_right  = "Delivered 29.02.2020";
 
-// This image is referenced in the meta tag
-$preview_image = "../../portfolio_2019_story/_chapters/intro/02.jpg";
+// This image is referenced in the meta tag, for rich embeds
+$preview_image = "";
 
 // _chapters
 // FORMAT: "Chapter Title", "link to chapter", "URL friendly title"
 $chapter_list  = array(
-    array("intro", "../../projects/intro/page.php",  "intro"),
-    array("help", "../../projects/help/page.php",  "help"),
-    array("radio", "../../projects/radio/page.php",  "radio"),
-
+    array("sample", "../../projects/sample/page.php",  "sample"),
+    array("sample", "../../projects/sample/page.php",  "sample"),
 );
 $chapter_count = count($chapter_list);
 ?>

92bcf7e61e5973f66597bf77e1c7bf3023f91acb
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-03 01:29:27 +0100

    minify

--- system/header.php
+++ system/header.php
@@ -13,6 +13,6 @@
 <meta property="og:url" content="https://moor.house" />
 <meta property="og:image" content="<?php echo($preview_image) ?>" />
 <!-- styles & tools -->
-<link rel="stylesheet" href="../../system/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-<link rel="stylesheet" href="../../system/css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="../../system/overwrite.css">
 <script src="https://kit.fontawesome.com/3850750bb2.js"></script>
